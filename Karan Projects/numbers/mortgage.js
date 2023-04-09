a49e39c883ffabf1e432d6a94eb8eb5ebf402429
mrstijket <mr.stijket@gmail.com>
2023-04-09 14:25:49 +0300

    Update mortgage.js

--- Karan Projects/numbers/mortgage.js
+++ Karan Projects/numbers/mortgage.js
@@ -3,24 +3,45 @@
 // Also figure out how long it will take the user to pay back the loan.For added complexity,
 // add an option for users to select the compounding interval(Monthly, Weekly, Daily, Continually).
 
-let totalMortgage = parseInt(prompt("Please enter total amount of mortgage(without comma or dot): "));
-let mortgageTime = parseInt(prompt("Please enter mortgage time in months: "));
-let interestRate = parseFloat(prompt("Please enter yearly interest rate: "));
-let typeOfLoan = prompt("Type of loan(continually, compley, left blank for basic): ")
+function mortgageCalculator(principal, term, rate, compoundingInterval) {
+    const n = term * compoundingInterval; // number of compounding periods
+    const r = rate / compoundingInterval / 100; // monthly interest rate
+    const payment =
+        (principal * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1); // monthly payment
 
-function calculateMonthlyPay(x) {
-    let rawMonthlyBill = totalMortgage / mortgageTime;
-    let monthlyInterest = Math.pow((1 + interestRate), (1 / 4)) - 1;
-    if (x == "continually") {
-        return totalMortgage + (totalMortgage * Math.log(1 + interestRate));
-    }
-    else if (x == "complex") {
-        return rawMonthlyBill + (rawMonthlyBill * monthlyInterest);
-    }
-    else {
-        return rawMonthlyBill * (interestRate / 12);
-    }
+    // calculate total payments and total interest paid
+    let totalPayments = payment * n;
+    let totalInterest = totalPayments - principal;
 
+    // format output
+    const formatter = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+    });
+
+    return {
+        payment: formatter.format(payment),
+        totalPayments: formatter.format(totalPayments),
+        totalInterest: formatter.format(totalInterest),
+        term: term,
+    };
 }
 
-console.log(calculateMonthlyPay(typeOfLoan.toLowerCase()));
+// Example usage
+const principal = 100000;
+const term = 10;
+const rate = 1.33;
+const compoundingInterval = 1; // monthly compounding
+
+const result = mortgageCalculator(
+    principal,
+    term,
+    rate,
+    compoundingInterval
+);
+
+console.log(`Monthly payment: ${result.payment}`);
+console.log(`Total payments: ${result.totalPayments}`);
+console.log(`Total interest paid: ${result.totalInterest}`);
+console.log(`Loan term: ${result.term} years`);
